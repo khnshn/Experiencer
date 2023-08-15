b62f08ea15529f0b80270d1685122f51c781f3b3
khnshn <programming.khanshan@gmail.com>
2023-08-15 10:05:21 +0200

    update to the latest stable version

--- src/config/gb.js
+++ src/config/gb.js
@@ -1,9 +1,9 @@
 var config = {
-  BASE_URL: "https://api4.gamebus.eu/v2", //"https://api4.gamebus.eu/v2", //"http://192.168.0.106:8024/v2", //"https://api3.gamebus.eu/v2",
+  BASE_URL: "https://api.gamebus.eu/v2", //"https://api4.gamebus.eu/v2", //"http://192.168.0.106:8024/v2", //"https://api3.gamebus.eu/v2",
   CONFIG_ENDPOINT: "/wearables/tizen/config",
   ACITIVTY_ENDPOINT: "/activities?dryrun=false",
   SNOOZE_ENDPOINT: "/wearables/tizen/snooze",
-  WIFI_INTERVAL: 300000,
+  WIFI_INTERVAL: 60000,
   SENSOR_RECORDING: 60000,
   ALARM_PERIOD: 30,
   PPG_INTERVAL: 10,
@@ -11,5 +11,4 @@ var config = {
   PEDOMETER_INTERVAL: 60000,
   TIZEN_PUSH: false,
   SYNC_STEPS: false,
-  AUTH_TOKEN: "your token goes here",
 };
